0b3b0cb80ec6564a2397a05fc6e2a8649fe690d4
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-12-29 19:19:41 +0100

    feat(theme): add custom Chakra UI theme and update dropdown styles

--- images/frontend/src/main.jsx
+++ images/frontend/src/main.jsx
@@ -5,11 +5,12 @@ import { ChakraProvider} from "@chakra-ui/react";
 
 import "./app.css";
 import App from "./App.jsx";
+import theme from "./theme.js";
 
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<ChakraProvider >
+		<ChakraProvider theme={theme}>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
